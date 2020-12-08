6e19fe9b00d59f297c97f2fd6671c412fb92ed2e
Franck <franck.siguier@gmail.com>
2020-12-08 14:26:54 +0100

    Added a validation on the format of the URL of the image for gardens and plants. The Regex is far too simple but it is enough for the purpose of this exercise

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -3,4 +3,8 @@ class Garden < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :banner_url, presence: true
+
+  url_regex = url_regex = /(http|https):.*/
+
+  validates :banner_url, format: { with: url_regex, message: "You have to paste a valid URL for the image" }
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -3,4 +3,8 @@ class Plant < ApplicationRecord
 
   validates :name, presence: true, uniqueness: { scope: :garden_id }
   validates :image_url, presence: true
+
+  url_regex = /(http|https):.*/
+
+  validates :image_url, format: { with: url_regex, message: "You have to paste a valid URL for the image" }
 end
